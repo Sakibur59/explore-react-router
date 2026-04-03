e291e3580fcf4710e1d40ccac25b801fc12d9d2c
Sakibur59 <sakiburrahman5978@gmail.com>
2026-04-03 16:09:24 +0600

    Explore Navigation

--- src/Components/Root/Root.jsx
+++ src/Components/Root/Root.jsx
@@ -1,15 +1,20 @@
-import React from 'react';
-import { Outlet } from 'react-router';
-import Header from '../Header/Header';
-import './Root.css'
+import React from "react";
+import { Outlet, useNavigation } from "react-router";
+import Header from "../Header/Header";
+import "./Root.css";
 
 const Root = () => {
-    return (
-        <div>
-            <Header></Header>
-            <Outlet></Outlet>
-        </div>
-    );
+  const navigation = useNavigation();
+  const isNavigating = Boolean(navigation.location);
+
+
+  return (
+    <div>
+      <Header></Header>
+        {isNavigating && <span>Loading....</span>}
+      <Outlet></Outlet>
+    </div>
+  );
 };
 
 export default Root;
